8c0023140dabaf778908dbf9a6fab1356fb66d3a
Song Young-jin <myevan80@gmail.com>
2014-03-08 15:32:06 +0900

    ! flask_ext_environment renamed to environment @ roesetta

--- rosetta/framework/__init__.py
+++ rosetta/framework/__init__.py
@@ -1,8 +1,6 @@
-import os
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_ext_environment import Environment
+from environment import Environment
 
 db = SQLAlchemy()
 env = Environment()
