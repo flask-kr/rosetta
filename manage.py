39efcb7206d96950d8713a0a20fd39530921831b
Song Young-jin <myevan80@gmail.com>
2014-03-09 14:20:14 +0900

    + 스마트 파일 경로 찾기 + CONFIG_DIR_PATH + ALEMBIC_REVISION_DIR_PATH @ manage.py

--- manage.py
+++ manage.py
@@ -8,6 +8,8 @@ from urlparse import urlparse
 
 from pypm import ProjectManager
 
+CONFIG_DIR_PATH = os.path.expandvars(
+    '$PROJECT_DIR/etc/configs')
 
 DEFAULT_CONFIG_FILE_PATH = os.path.expandvars(
     '$PROJECT_DIR/etc/configs/default_config.yml')
@@ -17,6 +19,10 @@ USER_CONFIG_FILE_PATH = os.path.expandvars(
 
 ALEMBIC_CONFIG_FILE_PATH = os.path.expandvars(
     '$PROJECT_DIR/alembic.ini')
+
+ALEMBIC_REVISION_DIR_PATH = os.path.expandvars(
+    '$PROJECT_DIR/alembic/versions')
+
 
 def create_application(custom_config_file_path=USER_CONFIG_FILE_PATH):
     if os.access(custom_config_file_path, os.R_OK):
@@ -46,14 +52,31 @@ def install_package(package_names):
     pm.run_system_command(
         'pip', ['freeze', '> ./requirements.txt'])
 
-@pm.command(config_file_path=dict(type=str, nargs=1, help='설정 파일 경로'))
-def switch_config(config_file_path):
+
+@pm.command(config_hint=dict(type=str, nargs=1, help='설정 파일 경로 혹은 접두어'))
+def switch_config(config_hint):
     """
     유저 설정 파일을 교체합니다.
     """
 
+    config_file_path = pm.smart_find_file_path(
+        config_hint, base_dir_path=CONFIG_DIR_PATH)
+
     pm.run_system_command(
         'cp', [config_file_path, USER_CONFIG_FILE_PATH])
+
+
+@pm.command(config_hint=dict(type=str, nargs=1, help='설정 파일 경로 혹은 접두어'))
+def edit_config(config_hint):
+    """
+    설정 파일을 수정 합니다.
+    """
+
+    config_file_path = pm.smart_find_file_path(
+        config_hint, base_dir_path=CONFIG_DIR_PATH)
+
+    pm.run_system_command('$EDITOR', [config_file_path])
+
 
 @pm.command(script_file_path=dict(type=str, nargs=1, help='스크립트 파일 경로'))
 def run_script(script_file_path):
@@ -66,25 +89,31 @@ def run_script(script_file_path):
     sys.path.append(script_dir_path)
     execfile(script_file_path, {'__name__': '__main__'})
 
-@pm.command(config_file_path=dict(type=str, flag='-c',
-                                  default=USER_CONFIG_FILE_PATH,
-                                  help='설정 파일 경로'))
-def run_shell(config_file_path):
+
+@pm.command(config_hint=dict(type=str, flag='-c',
+                             default=USER_CONFIG_FILE_PATH, help='설정 파일 경로'))
+def run_shell(config_hint):
     """
     쉘을 실행합니다. app 과 db 에 접근할 수 있습니다.
     """
 
+    config_file_path = pm.smart_find_file_path(
+        config_hint, base_dir_path=CONFIG_DIR_PATH)
+
     app = create_application(config_file_path)
     pm.run_python_shell('Rosetta Shell', local_dict=dict(app=app, db=db))
 
-@pm.command(config_file_path=dict(type=str, flag='-c',
-                                  default=USER_CONFIG_FILE_PATH,
-                                  help='설정 파일 경로'))
-def reset_all_dbs(config_file_path):
+
+@pm.command(config_hint=dict(type=str, flag='-c',
+                             default=USER_CONFIG_FILE_PATH, help='설정 파일 경로'))
+def reset_all_dbs(config_hint):
     """
     모든 데이터 베이스를 리셋합니다. 만약에 대비해 패스워드를 확인합니다.
     전체 리셋 패스워드를 지정하지 않았다면 사용할 수 없습니다.
     """
+
+    config_file_path = pm.smart_find_file_path(
+        config_hint, base_dir_path=CONFIG_DIR_PATH)
 
     app = create_application(config_file_path)
 
@@ -96,11 +125,11 @@ def reset_all_dbs(config_file_path):
     print "* reset_all_password:", 
     config_password = app.config['DB_RESET_ALL_PASSWORD']
     if not config_password:
-        raise pm.Error('NO_PERMISSION_TO_RESET_ALL_PASSWORD')
+        raise pm.Error('NOT_FOUND_TO_DB_RESET_ALL_PASSWORD_IN_CONFIG_PATH:' + config_file_path)
 
     input_password = raw_input()
     if input_password != config_password:
-        raise pm.Error('WRONG_RESET_ALL_PASSWORD')
+        raise pm.Error('WRONG_DB_RESET_ALL_PASSWORD')
 
     db.drop_all()
     db.create_all()
@@ -112,10 +141,12 @@ def reset_all_dbs(config_file_path):
     command.stamp(alembic_config, "head")
 
 
-@pm.command(config_file_path=dict(type=str, flag='-c',
-                                  default=USER_CONFIG_FILE_PATH,
-                                  help='설정 파일 경로'))
-def connect_db(config_file_path):
+@pm.command(config_hint=dict(type=str, flag='-c',
+                             default=USER_CONFIG_FILE_PATH, help='설정 파일 경로'))
+def connect_db(config_hint):
+    config_file_path = pm.smart_find_file_path(
+        config_hint, base_dir_path=CONFIG_DIR_PATH)
+
     app = create_application(config_file_path)
     db_uri = urlparse(app.config['SQLALCHEMY_DATABASE_URI'])
     if db_uri.scheme == 'sqlite':
@@ -128,47 +159,48 @@ def connect_db(config_file_path):
 def make_db_rev(title):
     pm.run_system_command('alembic', ['revision', '--autogenerate', '-m', title])
 
+
 @pm.command()
 def list_db_revs():
-    pm.run_system_command('ls', ['alembic/versions'])
+    pm.run_system_command('ls', [ALEMBIC_REVISION_DIR_PATH])
 
-@pm.command(prefix=dict(type=str, nargs=1, help='DB 리비전 파일 이름 접두어'))
-def edit_db_rev(prefix):
-    db_rev_pattern = prefix + '*'
-    for db_revision_file_path in pm.find_file_path_iter('alembic/versions', path_patterns=[db_rev_pattern]):
-        pm.run_system_command('$EDITOR', [db_revision_file_path])
-        break
-    else:
-        print 'NOT_EDITED_DB_REV:' + db_rev_pattern
 
-@pm.command(prefix=dict(type=str, nargs=1, help='DB 리비전 파일 이름 접두어'))
-def remove_db_rev(prefix):
-    db_rev_pattern = prefix + '*'
-    for db_revision_file_path in pm.find_file_path_iter('alembic/versions', path_patterns=[db_rev_pattern]):
-        pm.remove_file(db_revision_file_path, is_testing=False)
-        break
-    else:
-        print 'NOT_REMOVED_DB_REV:' + db_rev_pattern
+@pm.command(db_rev_hint=dict(type=str, nargs=1, help='DB 리비전 파일 이름 접두어'))
+def edit_db_rev(db_rev_hint):
+    db_rev_file_path = pm.smart_find_file_path(
+        db_rev_hint, base_dir_path=ALEMBIC_REVISION_DIR_PATH)
 
-@pm.command(prefix=dict(type=str, nargs=1, help='DB 리비전 파일 이름 접두어'))
-def apply_db_rev(prefix):
-    db_rev_pattern = prefix + '*'
-    for db_revision_file_path in pm.find_file_path_iter('alembic/versions', path_patterns=[db_rev_pattern]):
-        db_revision_file_name = os.path.basename(db_revision_file_path).split('_')[0]
-        pm.run_system_command('alembic', ['upgrade', db_revision_file_name])
-        break
-    else:
-        print 'NOT_APPLIED_DB_REV:' + db_rev_pattern
+    pm.run_system_command('$EDITOR', [db_rev_file_path])
 
-@pm.command(config_file_path=dict(type=str, flag='-c',
-                                  default=USER_CONFIG_FILE_PATH,
-                                  help='설정 파일 경로'),
+
+@pm.command(db_rev_hint=dict(type=str, nargs=1, help='DB 리비전 파일 이름 접두어'))
+def remove_db_rev(db_rev_hint):
+    db_rev_file_path = pm.smart_find_file_path(
+        db_rev_hint, base_dir_path=ALEMBIC_REVISION_DIR_PATH)
+
+    pm.remove_file(db_rev_file_path, is_testing=False)
+
+
+@pm.command(db_rev_hint=dict(type=str, nargs=1, help='DB 리비전 파일 이름 접두어'))
+def apply_db_rev(db_rev_hint):
+    db_rev_file_path = pm.smart_find_file_path(
+        db_rev_hint, base_dir_path=ALEMBIC_REVISION_DIR_PATH)
+
+    db_rev_file_name = os.path.basename(db_rev_file_path).split('_')[0]
+    pm.run_system_command('alembic', ['upgrade', db_rev_file_name])
+
+
+@pm.command(config_hint=dict(type=str, flag='-c',
+                             default=USER_CONFIG_FILE_PATH,
+                             help='설정 파일 경로'),
             port=dict(type=int, flag='-p', default=5000, help="포트 번호"))
-def run_server(config_file_path, port):
+def run_server(config_hint, port):
     """
     서버를 실행합니다. 기본 포트는 5000번입니다.
     """
 
+    config_file_path = pm.smart_find_file_path(
+        config_hint, base_dir_path=CONFIG_DIR_PATH)
     app = create_application(config_file_path)
     app.run(port=port)
 
