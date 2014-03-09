e2d2b0f86cea5f37032c33da7c740bf18fd9f6eb
Song Young-jin <myevan80@gmail.com>
2014-03-09 23:46:03 +0900

    ! 기능 테스트 관련 코드 정리 @ tests

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -2,6 +2,7 @@
 
 from BeautifulSoup import BeautifulSoup, NavigableString
 
+
 def translate_html(html_data, text_dict):
     soup = BeautifulSoup(html_data)
     html_texts = soup.findAll(text=True)
@@ -17,10 +18,13 @@ def translate_html(html_data, text_dict):
     return soup
 
 if __name__ == '__main__':
-    # import urllib
-    # html_data = urllib.urlopen(
-    # 'https://github.com/liks79/flask/'
-    # 'blob/docs-korean/docs/en/advanced_foreword.rst').read()
-    # open('test.html', 'wb').write(html_data)
-    html_data = open('test.html', 'rb').read()
-    translate_html(html_data, {u'Sign up': u'가입하기'})
+    def main():
+        # import urllib
+        # html_data = urllib.urlopen(
+        # 'https://github.com/liks79/flask/'
+        # 'blob/docs-korean/docs/en/advanced_foreword.rst').read()
+        # open('test.html', 'wb').write(html_data)
+        html_data = open('test.html', 'rb').read()
+        translate_html(html_data, {u'Sign up': u'가입하기'})
+
+    main()
